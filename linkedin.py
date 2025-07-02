c32f2621f6299e2e6ce95e2859063a0ac179f14b
Ana Carolina Ferreira Camargos <Ana52275166@edu.df.senac.br>
2025-07-02 17:28:39 -0300

    corrigindo erros

--- linkedin.py
+++ linkedin.py
@@ -13,16 +13,9 @@ pyautogui.click()
 time.sleep(1)
 pyautogui.moveTo(619, 243, duration=1)
 pyautogui.click()
-<<<<<<< HEAD
 texto = "🎆 Olá pessoal, estou aprendendo a realizar automações simples e essa mensagem foi feita por uma automação que foi ensinada pelo meu professor @Gessiel Silva, estamos iniciando esse conteúdo e é louco o quando a tecnologia pode realizar. Isso é apenas uma das várias coisas que a automação em Python pode realizar. Link do meu projeto no Git Hub: https://github.com/anacferc/automacoespy/blob/main/linkedin.py "
-=======
-texto = "🎆 Olá pessoal, estou aprendendo a realizar automações simples e essa mensagem foi feita por uma automação que foi ensinada pelo meu professor @Gessiel Silva, estamos iniciando esse conteúdo e é louco o quando a tecnologia pode realizar. Isso é apenas uma das várias coisas que a automação em Python pode realizar. Link do meu projeto no Git Hub: https://github.com/anacferc/automacoes/blob/main/linkedin.py "
->>>>>>> 00c8d3d9ad37c0430030ad8e425dac478863f174
 pyperclip.copy(texto)
 time.sleep(1)
 pyautogui.hotkey("ctrl", "v")
 pyautogui.moveTo(1258, 688, duration=1)
-<<<<<<< HEAD
 pyautogui.click()
-=======
->>>>>>> 00c8d3d9ad37c0430030ad8e425dac478863f174
